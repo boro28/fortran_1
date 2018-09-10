18064c96240e66ec804d1bf533ebe283609250f0
Szymon Borkowski <borkowskiszymon28@gmail.com>
2018-09-11 01:59:04 +0200

    gauss done

--- gauss.f90
+++ gauss.f90
@@ -1,22 +1,25 @@
 module gauss
-  use utils
-  implicit none
+    use utils
+    implicit none
 
-  contains
-  subroutine eliminate(matrix, vector, size)
+contains
+    subroutine eliminate(matrix, vector, size)
         integer, intent(in) :: size
-        real (kind = iPRECISION), dimension(size, size) :: matrix
-        real (kind = iPRECISION), dimension(size) :: vector
+        real (kind = precision), dimension(size, size) :: matrix
+        real (kind = precision), dimension(size) :: vector
 
         integer :: i, j
-        real (kind = iPRECISION) :: multiplier
+        real (kind = precision) :: multiplier
         do i = 1, size
-            multiplier = matrix(i, j) / matrix(i, i)
-            matrix(:, j) = matrix(:, j) - multiplier * matrix(:, i)
-            vector(j) = vector(j) - multiplier * vector(i)
-            vector(i) = vector(i) / matrix(i, i)
-                matrix(:, i) = matrix(:, i) / matrix(i, i)
+            do j = i+1, size
+                if(matrix(i, i) /= 0) then
+                    multiplier = matrix(i, j) / matrix(i, i)
+                    matrix(:, j) = matrix(:, j) - multiplier * matrix(i, :)
+                    vector(j) = vector(j) - multiplier * vector(i)
+                    vector(i) = vector(i) / matrix(i, i)
+                    matrix(:, i) = matrix(:, i) / matrix(i, i)
+                end if
             end do
         end do
-  end subroutine eliminate
+    end subroutine eliminate
 end module gauss
