dcb42caff97537c569d8080a8f1a2e7314e6d57b
Szymon Borkowski <borkowskiszymon28@gmail.com>
2018-09-11 11:46:37 +0200

    Small fix

--- gauss.f90
+++ gauss.f90
@@ -14,7 +14,7 @@ contains
             do j = i+1, size
                 if(matrix(i, i) /= 0) then
                     multiplier = matrix(i, j) / matrix(i, i)
-                    matrix(:, j) = matrix(:, j) - multiplier * matrix(i, :)
+                    matrix(:, j) = matrix(:, j) - multiplier * matrix(:, i)
                     vector(j) = vector(j) - multiplier * vector(i)
                     vector(i) = vector(i) / matrix(i, i)
                     matrix(:, i) = matrix(:, i) / matrix(i, i)
